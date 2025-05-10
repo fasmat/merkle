d327a232072074bed34f1d2f554053d9ca744408
Matthias <5011972+fasmat@users.noreply.github.com>
2025-05-10 16:01:19 +0000

    Use pointer receivers to ensure we avoid unnecessary copies

--- internal/ordered/doc.go
+++ internal/ordered/doc.go
@@ -1,2 +1,6 @@
 // Package ordered is a package that implements an ordered map data structure.
+//
+// It maintains the order of keys based on their natural ordering and provides
+// methods to retrieve values, keys, and the length of the map as well as an
+// iterable sequence of all key-value pairs.
 package ordered
